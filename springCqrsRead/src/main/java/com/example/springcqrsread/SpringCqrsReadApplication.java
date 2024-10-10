6c00239b385707643f810f091194dbd7836dbb9b
yeonsu <bestdustn@sookmyung.ac.kr>
2024-10-10 15:26:59 +0900

    [Add] initial commit

---
+++ springCqrsRead/src/main/java/com/example/springcqrsread/SpringCqrsReadApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcqrsread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCqrsReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCqrsReadApplication.class, args);
+    }
+
+}
---
+++ springCqrsRead/src/test/java/com/example/springcqrsread/SpringCqrsReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcqrsread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCqrsReadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
