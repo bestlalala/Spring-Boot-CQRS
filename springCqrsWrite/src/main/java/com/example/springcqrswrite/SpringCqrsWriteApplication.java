2990b828908e4e276403509b223b5df65b7825ec
yeonsu <bestdustn@sookmyung.ac.kr>
2024-10-12 18:39:29 +0900

    [Add] cqrs write initial commit

---
+++ springCqrsWrite/src/main/java/com/example/springcqrswrite/SpringCqrsWriteApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcqrswrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCqrsWriteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCqrsWriteApplication.class, args);
+    }
+
+}
